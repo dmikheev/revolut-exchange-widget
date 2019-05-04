d7b828f7d6d233ef703ada12af552c66d42f8d85
Dmitry Mikheev <medes111@gmail.com>
2019-05-04 21:07:30 +0300

    Implement api

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import erApi from '../api/exchangeRatesApi';
+import { Currency } from '../constants/currencies';
 import styles from './App.module.css';
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    erApi.get(Currency.USD).then((res) => console.log(res));
+  }, []);
+
   return (
     <div className={styles.app}>
       <h1>Exchange widget demo</h1>

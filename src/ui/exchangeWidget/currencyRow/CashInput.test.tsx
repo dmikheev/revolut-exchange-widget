56b3325cf829c013ac07e88ab81aac2951bfd4bd
Dmitry Mikheev <medes111@gmail.com>
2019-06-03 21:54:09 +0300

    Test CashInput change

--- src/ui/exchangeWidget/currencyRow/CashInput.test.tsx
+++ src/ui/exchangeWidget/currencyRow/CashInput.test.tsx
@@ -1,7 +1,21 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
 import CashInput from './CashInput';
 
 it('shallow renders without crashing', () => {
   shallow(<CashInput/>);
+});
+
+it('calls onChange only with correct values', () => {
+  const onChange = jest.fn();
+  const cashInput = mount(<CashInput onChange={onChange}/>);
+
+  const textFieldFind = cashInput.find('input');
+  expect(textFieldFind).toHaveLength(1);
+
+  textFieldFind.simulate('change', { target: { value: 'abc' } });
+  expect(onChange).not.toHaveBeenCalled();
+
+  textFieldFind.simulate('change', { target: { value: '12.34' } });
+  expect(onChange).toHaveBeenCalledTimes(1);
 });

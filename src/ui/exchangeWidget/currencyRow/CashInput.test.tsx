925365305d118970146a6aa22d44ffa63f7ee50b
Dmitry Mikheev <medes111@gmail.com>
2019-06-04 00:00:13 +0300

    Fix var name in CashInput test

--- src/ui/exchangeWidget/currencyRow/CashInput.test.tsx
+++ src/ui/exchangeWidget/currencyRow/CashInput.test.tsx
@@ -10,12 +10,12 @@ it('calls onChange only with correct values', () => {
   const onChange = jest.fn();
   const cashInput = mount(<CashInput onChange={onChange}/>);
 
-  const textFieldFind = cashInput.find('input');
-  expect(textFieldFind).toHaveLength(1);
+  const inputFind = cashInput.find('input');
+  expect(inputFind).toHaveLength(1);
 
-  textFieldFind.simulate('change', { target: { value: 'abc' } });
+  inputFind.simulate('change', { target: { value: 'abc' } });
   expect(onChange).not.toHaveBeenCalled();
 
-  textFieldFind.simulate('change', { target: { value: '12.34' } });
+  inputFind.simulate('change', { target: { value: '12.34' } });
   expect(onChange).toHaveBeenCalledTimes(1);
 });
